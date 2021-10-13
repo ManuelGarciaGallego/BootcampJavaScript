859d83fed7de18aa5631e44db6842e5039bbeda8
Manuel García <manuelgalleggo@gmail.com>
2021-10-13 12:37:55 +0200

    Laboratorio

--- Módulo 1 Introducción/Introducción-Laboratorio/js/demo.js
+++ Módulo 1 Introducción/Introducción-Laboratorio/js/demo.js
@@ -1,3 +1,3 @@
 document.getElementById("name").value = "Manuel";
 document.getElementById("lastname").value = "García"
-document.getElementById("avatar").src ="img/avatar prácticas.jpeg"
+document.getElementById("avatar").src ="img/avatarprácticas.jpeg"
