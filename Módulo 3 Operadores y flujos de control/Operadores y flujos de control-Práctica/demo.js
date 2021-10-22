dcad80073d208881e63cc1082486312db540e515
Manuel García <manuelgalleggo@gmail.com>
2021-10-22 16:29:19 +0200

    Práctica

---
+++ Módulo 3 Operadores y flujos de control/Operadores y flujos de control-Práctica/demo.js
@@ -0,0 +1,48 @@
+const booking = { count: 5, price: 127.95 };
+
+
+let total = booking.count * booking.price;
+
+
+// let discount = total * 0.2;
+
+
+// if (total > 500) {
+//      total = total - discount; 
+
+// }else {
+//     total = total;
+// }
+  
+
+//  ternario de total
+// total = total > 500 ? total - discount : total  
+
+// console.log(total);
+
+console.log("Total de la reserva sin descuento:", total + "€")
+
+// if (total > 500 && discount > 0) {
+//     console.log("Descuento aplicado:", discount + "€")
+//     console.log("Total con descuento:", total - discount + "€")
+// }
+
+//   ternario de discount
+discount = total > 500 ? total * 0.20 : 0;
+console.log("Descuento:", discount + "€");
+
+
+if (total > 500 && discount > 0) {
+    const disconted = total - discount;
+    console.log("Total con descuento:", disconted + "€");
+
+}
+
+
+
+
+
+
+
+
+
