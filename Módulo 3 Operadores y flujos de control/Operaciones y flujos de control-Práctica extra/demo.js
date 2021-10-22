4ee7d634b2ca40cc3f869d7e5ce5745ae51f7c72
Manuel García <manuelgalleggo@gmail.com>
2021-10-22 20:05:30 +0200

    Práctica extra

---
+++ Módulo 3 Operadores y flujos de control/Operaciones y flujos de control-Práctica extra/demo.js
@@ -0,0 +1,36 @@
+const booking1 = { count: 3, price: 127.95 };
+const booking2 = { count: 5, price: 112.95 };
+
+// if (booking1.count = booking2.count && booking1.price == booking2.price) {
+//     console.log("Son iguales");
+// }else {
+//     console.log("No son iguales")
+
+// };
+
+
+// const totalnoches = booking1.count + booking2.count;
+// console.log("Número total de noches:", totalnoches);
+
+// console.log("Número total de noches:", booking1.count + booking2.count);
+
+
+// const Númeronoches = booking1.count > booking2.count ? booking1 : booking2;
+// console.log("Reserva con mayor número de noches",Númeronoches);
+
+
+const Ampliaciónreserva = (booking1.count++);
+console.log(Ampliaciónreserva, booking1.count);
+
+
+
+
+
+
+
+
+
+
+
+
+
