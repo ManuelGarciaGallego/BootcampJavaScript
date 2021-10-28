e59ff1682ebb6e5e1d058bc275ad6b087f199961
Manuel García <manuelgalleggo@gmail.com>
2021-10-28 14:25:48 +0200

    Calcular el área según el tipo de figura geométrica

---
+++ Módulo 3 Operadores y flujos de control/Operadores y flujos de control-Calcular el área según el tipo de figura geométrica/app.js
@@ -0,0 +1,32 @@
+var square = {
+    sideOne: 1,
+    sidetwo: 3,
+    typeOfGeometricFigure: "Cuadrado",
+
+}
+
+var circle = {
+    radius: 5,
+    typeOfGeometricFigure: "Circulo",
+
+}
+
+
+squareArea = square.sideOne * square.sidetwo;
+
+const Pi = 3.14;
+circleArea = Pi * circle.radius;
+
+
+let typeOfGeometricFigure;
+
+if (typeOfGeometricFigure = "Cuadrado") {
+    console.log("Área del cuadrado:", typeOfGeometricFigure = squareArea);
+
+}
+
+if (typeOfGeometricFigure = "Circulo") {
+    console.log("Área del circulo:", typeOfGeometricFigure = circleArea);
+    
+}
+
