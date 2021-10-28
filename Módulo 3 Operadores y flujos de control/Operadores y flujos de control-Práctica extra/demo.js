d0bcbda8002c790692e3941273f4025543676a3b
Manuel García <manuelgalleggo@gmail.com>
2021-10-28 12:16:06 +0200

    Práctica extra

--- Módulo 3 Operadores y flujos de control/Operadores y flujos de control-Práctica extra/demo.js
+++ Módulo 3 Operadores y flujos de control/Operadores y flujos de control-Práctica extra/demo.js
@@ -1,30 +1,30 @@
 const booking1 = { count: 3, price: 127.95 };
 const booking2 = { count: 5, price: 112.95 };
 
-// if (booking1.count = booking2.count && booking1.price == booking2.price) {
-//     console.log("Son iguales");
-// }else {
-//     console.log("No son iguales");
+if (booking1.count = booking2.count && booking1.price == booking2.price) {
+    console.log("Son iguales");
+}else {
+    console.log("No son iguales");
 
-// }
+}
 
 
-// const totalnoches = booking1.count + booking2.count;
-// console.log("Número total de noches:", totalnoches);
+const totalnoches = booking1.count + booking2.count;
+console.log("Número total de noches:", totalnoches);
 
 // console.log("Número total de noches:", booking1.count + booking2.count);
 
 
-// const Númeronoches = booking1.count > booking2.count ? booking1 : booking2;
-// console.log("Reserva con mayor número de noches",Númeronoches);
+const Númeronoches = booking1.count > booking2.count ? booking1 : booking2;
+console.log("Reserva con mayor número de noches",Númeronoches);
 
 
-// const Ampliaciónreserva = (booking1.count++);
-// console.log(Ampliaciónreserva, booking1.count);
+const Ampliaciónreserva = (booking1.count++);
+console.log(Ampliaciónreserva, booking1.count);
 
 
 
-// let ReservaConDescuento = 0;
+let ReservaConDescuento = 0;
 
 
 if (booking1.count * booking1.price > 500) {
